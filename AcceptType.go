fcb29d544f98ed2533acbb2c19e92bfe5123984f
Nick Collins <nick.m.collins@gmail.com>
2021-05-21 19:37:48 -0500

    AcceptType now waits for a success response back

--- AcceptType.go
+++ AcceptType.go
@@ -1,6 +1,7 @@
 package clientlib
 
 import (
+	"errors"
 	"github.com/adrianleh/WTMP-middleend/command"
 	"github.com/adrianleh/WTMP-middleend/types"
 )
@@ -13,5 +14,27 @@ func AcceptType(typ types.Type) error {
 		return headerErr
 	}
 	msg = append(msg, typSer...)
-	return sendViaSocket(msg)
+
+	commLock.Lock()
+	defer commLock.Unlock()
+
+	if err := sendViaSocket(msg); err != nil {
+		return err
+	}
+
+	recvConnLock.Lock()
+	for recvConn == nil {
+		recvConnCond.Wait()
+	}
+	recvConnLock.Unlock()
+
+	success := make([]byte, 1)
+	if _, err := recvConn.Read(success); err != nil {
+		return err
+	}
+	if success[0] == 0 {
+		return nil
+	} else {
+		return errors.New("middle-end rejected AcceptType command")
+	}
 }
